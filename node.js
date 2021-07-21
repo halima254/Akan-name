68388202d64ac177242524a39ffc3c87076b44a3
halima254 <halima.yahya@student.moringaschool.com>
2021-07-22 00:24:21 +0300

    add indention and coments

--- node.js
+++ node.js
@@ -1,12 +1,16 @@
+// variables for malenames, femalenames and weekdays
 const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+// a function that takes in variables used in the calculation of birthday
 function validatorFunction() {
     const year = parseInt(document.getElementById("year").value);
     const month = parseInt(document.getElementById("month").value);
     const date = parseInt(document.getElementById("date").value);
     const male = document.getElementById("male");
     const female = document.getElementById("female");
+
 
     if (year <=1800 || year >= 2022) {
         alert("Enter valid year");
@@ -18,10 +22,13 @@ function validatorFunction() {
         alert("Please fill the form");
     }
 
+//method calculating the day of the week.
     const a = Math.floor((14 - month) / 12)
     const y = year - a
     const m = month + 12 * a - 2
     const d = (date + y + Math.floor(y / 4) - Math.floor(y / 100) + Math.floor(year / 400) + Math.floor((31 * m) / 12)) % 7;
+
+//conditional statement that prints out the name and the day of the week.
     if (male.checked == true) {
         ("You were born on " + weekDays[d] + " your Akan name is: " + maleNames[d]);
         document.getElementById('name').innerHTML = "You were born on " + weekDays[d] + " and your Akan name is: " + maleNames[d];
